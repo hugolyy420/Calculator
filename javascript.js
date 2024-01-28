d34c3b1a4d582e209454f77eb289aba9503fe8a0
Hugo Leung <132837475+hugounique@users.noreply.github.com>
2024-01-29 00:55:04 +0800

    Create functions for populating the display

--- javascript.js
+++ javascript.js
@@ -1,3 +1,16 @@
+const buttons = document.querySelectorAll('input[type=button]');
+const displaybox = document.getElementById('result');
+
+buttons.forEach(button => {
+    button.addEventListener('click', display);
+})
+
+function display(e) {
+    let buttonclicked = e.target.value;
+  
+    displaybox.value = buttonclicked;
+  }
+
 function Calculator () {
 
     this.methods = {
